a9f3a143aba8cecdc2c078074c4fe436423b8ede
Dominique Audet <dominique.audet@cegeplimoilou.ca>
2023-08-27 12:54:03 -0400

    Updates

---
+++ precache-manifest.667c798feae9c76f1df5979ad4cb0b7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af91267d89188303d023c81f9d33272",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86484a3afc84bd0cd286",
+    "url": "/static/css/2.540ad93e.chunk.css"
+  },
+  {
+    "revision": "9826e5e842d9099089ec",
+    "url": "/static/css/main.3b492ebb.chunk.css"
+  },
+  {
+    "revision": "86484a3afc84bd0cd286",
+    "url": "/static/js/2.ee60ac9f.chunk.js"
+  },
+  {
+    "revision": "9fd8ac69f8779f68b807eb06f9423c10",
+    "url": "/static/js/2.ee60ac9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9826e5e842d9099089ec",
+    "url": "/static/js/main.bb81e5cc.chunk.js"
+  },
+  {
+    "revision": "4707fa85c5c3f8a24c02",
+    "url": "/static/js/runtime-main.170ce51c.js"
+  }
+]);
